43da5683398058d2c3e67da9fa4835dc16187f9f
Elliot Potts <elliot@skyzun.net>
2018-02-18 02:04:07 +0000

    Finished.
    This was a short project, but I made a mock up of it when I was attempting fixes for my Macbook Pro 2011.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 
 
 def moveFunc():
@@ -9,9 +10,16 @@ def moveFunc():
     print("Disabled extensions directory created in {}".format(str(os.getcwd())+"\Disabled_Ext"))
     os.system("mount -uw")
     os.system("sudo mv /System/Library/Extensions/AMD*.* /Disabled_Ext/")
+    os.system("sudo rm -rf /System/Library/Caches/com.apple.kext.caches/")
+    os.system("sudo mkdir /System/Library/Caches/com.apple.kext.caches/")
+    os.system("sudo touch /System/Library/Extensions/")
+    os.system("sudo umount /")
+    print("Files successfully moved. You should now be using Integrated Graphics.\nRebooting in 5 seconds.")
+    time.sleep(5)
+    os.system("sudo reboot")
 
 
 if sys.argv[1] == "-move":
     moveFunc()
 else:
-    print("You have entered an incorrect parameter.\nEither use python main.py -move")
+    print("You have entered an incorrect parameter.\nUse python main.py -move")
